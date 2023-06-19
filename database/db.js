6bc0509a54e54751b4dda427df6feedabc088233
vaibhav-vm-vidhya <vaibhavvakil1234@gmail.com>
2023-06-19 15:07:37 +0530

    problem Udated

--- database/db.js
+++ database/db.js
@@ -1,18 +1,20 @@
 const mongoose = require("mongoose");
+
 const mongoURI =
   "mongodb+srv://vibeuser1:vaibhav@cluster0.9qofxzb.mongodb.net/vibebackend?retryWrites=true&w=majority";
 // const mongoURI = "mongodb://localhost:27017/vibebackend";
 
+mongoose.set("strictQuery", true);
+
 const connectToMongo = () => {
-  mongoose.connect(mongoURI);
-
-  mongoose.connection.on("connected", () => {
-    console.log("Connected successfully");
-  });
-
-  mongoose.connection.on("error", (err) => {
-    console.error("MongoDB connection error:", err);
-  });
+  mongoose
+    .connect(mongoURI)
+    .then(() => {
+      console.log("Connected successfully");
+    })
+    .catch((error) => {
+      console.error("Error connecting to MongoDB:", error);
+    });
 };
 
 module.exports = connectToMongo;
